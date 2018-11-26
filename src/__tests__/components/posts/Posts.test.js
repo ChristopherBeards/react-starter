4cb8e12cc61753911c97d40fed27f18be77508d7
Christopher Beards <BeardsCM@gmail.com>
2018-11-26 17:01:01 -0500

    Posts.test.js; implemented further testing

--- src/__tests__/components/posts/Posts.test.js
+++ src/__tests__/components/posts/Posts.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Posts from '../../../components/posts/Posts';
 
 describe('<Posts />', () => {
-  it('Returns a list of posts', () => {
+  // Snapshot Test
+  it('Matches snapshot', () => {
     expect.assertions(1);
     expect(shallow(<Posts />)).toMatchSnapshot();
+  });
+
+  // Returns state as an array
+  it('Returns an array', () => {
+    const wrapper = mount(<Posts />);
+    expect(wrapper.state().posts).toEqual([]);
   });
 });
